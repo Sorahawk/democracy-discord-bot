a9d831b81c6d66d6554f2a8dd69cd08dc28b1324
Sorahawk <sorahawktech@gmail.com>
2024-08-03 15:12:11 +0800

    edited secrets_template.py to remove some unnecessary variables

--- python-scripts/secrets_template.py
+++ python-scripts/secrets_template.py
@@ -5,11 +5,5 @@
 # (boolean) Toggle development/debug mode
 DEBUG_MODE = False
 
-# (string) AWS Access Key
-AWS_ACCESS_KEY = None
-
-# (string) AWS Secret Key
-AWS_SECRET_KEY = None
-
 # (string) Discord Bot Token
 DISCORD_BOT_TOKEN = None
